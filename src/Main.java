bc7d9349f6836097e5547611c51271216b1b90a9
proliboon <proliboon@gmail.com>
2024-12-04 22:03:17 +0800

    removed sample comment

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         MutableAuctionSystemHolder auctionSystemHolder = new MutableAuctionSystemHolder();
         SwingUtilities.invokeLater(() -> new SellerGUI(auctionSystemHolder));
-        // sample comment
     }
 }
