fc94009773c879440dd70b809261a273d7a90d0c
JomarcOrogo <jomarcorogo07@gmail.com>
2024-12-04 21:37:36 +0800

    Update Main.java
    added some comments

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         MutableAuctionSystemHolder auctionSystemHolder = new MutableAuctionSystemHolder();
         SwingUtilities.invokeLater(() -> new SellerGUI(auctionSystemHolder));
+        // sample comment
     }
 }
